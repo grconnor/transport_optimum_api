443158d35a6c2c722ec577f8aeec70a6f065528f
SebastianN97 <sebbe132132@gmail.com>
2020-11-05 17:27:11 +0100

    Creating spec test. Configuring routes. Fixing mount devise naming.
    Co-authored-by: grconnor <connorroelofsen15@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'api/v0/auth', skip:
+  mount_devise_token_auth_for 'User', at: 'api/v1/auth', skip:
   [:omniauth_callback]
 
   namespace :api do
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    email { "user@mail.com" }
+    email { "user@mail#{rand(0..999)}.com" }
     password { "password" }
     password_confirmation { "password" }
   end
