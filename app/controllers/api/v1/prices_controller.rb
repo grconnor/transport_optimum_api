61154f17f1acd2e034aea4e919b8f9e2fe83b19e
Connor Roelofsen <connorroelofsen15@gmail.com>
2020-11-12 13:36:53 +0100

    Removes not needed line.

--- app/controllers/api/v1/prices_controller.rb
+++ app/controllers/api/v1/prices_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::PricesController < ApplicationController
 
   def create
     pricePerKm = 5 
-    distanceString = params[:distance]
     distanceInKm = (params[:distance].to_i)/1000
 
     totalPrice = (distanceInKm)*(pricePerKm)
